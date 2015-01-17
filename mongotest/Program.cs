6d4f7d4e7e4dec7a93bdefd95a0d0a0248c32b5b
ricekeller <ricekeller@gmail.com>
2015-01-17 10:21:33 -0600

    add mongotest
    add mongotest

---
+++ mongotest/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Builders;
+
+namespace mongotest
+{
+    public class Entity
+    {
+        public ObjectId Id { get; set; }
+        public string Name { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var connectionString = "mongodb://devtest:devtest@ds029541.mongolab.com:29541/devtest";
+            var client = new MongoClient(connectionString);
+            var server = client.GetServer();
+            var database = server.GetDatabase("devtest");
+            var collection = database.GetCollection<Entity>("entities");
+
+
+            //var entity = new Entity { Name = "Tom" };
+            //collection.Insert(entity);
+            //var id = entity.Id;
+
+            //var query = Query<Entity>.EQ(e => e.Id, id);
+            //entity = collection.FindOne(query);
+
+            //entity.Name = "Dick";
+            //collection.Save(entity);
+
+            //var update = Update<Entity>.Set(e => e.Name, "Harry");
+            //collection.Update(query, update);
+
+            //collection.Remove(query);
+            
+            for(int i=0;i<10000;i++)
+            {
+                var entity = new Entity() { Name = i.ToString() };
+                collection.Insert(entity);
+            }
+        }
+    }
+}
