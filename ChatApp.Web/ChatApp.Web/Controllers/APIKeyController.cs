beec48f7d8f5d6c042860fe33b7c26db7bebd78f
ricekeller <ricekeller@gmail.com>
2015-01-20 19:58:01 -0600

    sln created
    sln created

---
+++ ChatApp.Web/ChatApp.Web/Controllers/APIKeyController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ChatApp.Web.Controllers
+{
+    public class APIKeyController : ApiController
+    {
+        [HttpGet]
+        public string UpdateKey(string key)
+        {
+            return "123";
+        }
+    }
+}
