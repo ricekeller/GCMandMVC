d4104d7d5abe071f7962f13a4eb29e4a444d0c29
ricekeller <ricekeller@gmail.com>
2015-02-09 17:06:14 -0600

    ajax filter
    ajax filter

---
+++ ChatApp.Web/ChatApp.Web/Models/Attributes/CheckAjaxRequestAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace testAjaxReturn.Models
+{
+	public class CheckAjaxRequestAttribute : ActionFilterAttribute
+	{
+		private const string AJAX_HEADER = "X-Requested-With";
+
+		public override void OnActionExecuting(ActionExecutingContext filterContext)
+		{
+			bool isAjaxRequest = filterContext.HttpContext.Request.Headers[AJAX_HEADER] != null;
+			if (!isAjaxRequest)
+			{
+				filterContext.Result = new ViewResult { ViewName = "Unauthorized" };
+			}
+		}
+	}
+}
