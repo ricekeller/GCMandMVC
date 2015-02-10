de98bea7842f26f317d09bca9679c7d31f709091
ricekeller <ricekeller@gmail.com>
2015-02-10 14:15:46 -0600

    user-controller and view models
    user-controller and view models

---
+++ ChatApp.Web/ChatApp.Web/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ChatApp.Web.Models.ViewModels;
+
+namespace ChatApp.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        //
+        // GET: /Account/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+		[HttpGet]
+		public ActionResult Login()
+		{
+			return View(new LoginViewModel());
+		}
+
+		[HttpPost]
+		public ActionResult Login(LoginViewModel lvm)
+		{
+			if(ModelState.IsValid)
+			{
+				//do some login staff
+			}
+			return View(lvm);
+		}
+
+		[HttpGet]
+		public ActionResult Register()
+		{
+			return View(new CreateAccountViewModel());
+		}
+
+		[HttpPost]
+		public ActionResult Register(CreateAccountViewModel vm)
+		{
+			if(ModelState.IsValid)
+			{
+				//do some create staff
+			}
+			return View(vm);
+		}
+    }
+}
---
+++ ChatApp.Web/ChatApp.Web/Models/ViewModels/CreateAccountViewModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ChatApp.Web.Models.ViewModels
+{
+	public class CreateAccountViewModel:IValidatableObject
+	{
+		[Required]
+		[MinLength(6)]
+		[EmailAddress(ErrorMessage = "Invalid email address.")]
+		public string Email { get; set; }
+
+		[Required]
+		public string Password { get; set; }
+
+		[Required]
+		[Display(Name="Confirm Password")]
+		public string ConfirmPassword { get; set; }
+
+		[Display(Name="Display Name")]
+		[Range(2,20)]
+		public string DisplayName { get; set; }
+
+		public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+		{
+			if(null!=Password&&null!=ConfirmPassword&&!Password.Equals(ConfirmPassword))
+			{
+				yield return new ValidationResult("Confirm password is not the same with password", new string[] { "ConfirmPassword" });
+			}
+		}
+	}
+}
