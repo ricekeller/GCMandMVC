c23ad33e190d5bb2fcbf57adbfcfd1d7f8d9eab7
ricekeller <ricekeller@gmail.com>
2015-02-11 21:47:50 -0600

    ajax login and register
    ajax login and register

--- ChatApp.Web/ChatApp.Web/Content/Scripts/Layout.js
+++ ChatApp.Web/ChatApp.Web/Content/Scripts/Layout.js
@@ -1,21 +1,28 @@
-﻿$(function()
-{
+﻿$(function () {
     $("#menu").menu({
-        select:function(event,ui)
-        {
-        	var url = ui.item.attr("data-url");
-        	var postAction = ui.item.attr("data-postLoadAction")+"()";
-        	if (null !== url && url !== "")
-        	{
-        		$.get(url, function (data)
-        		{
-        			$("#mainContentContainer").html(data);
-        			if(null!==postAction&&postAction!==""&&postAction!=="undefined()")
-        			{
-        				eval(postAction);
-        			}
-        		});
-        	}
+        select: function (event, ui) {
+            var url = ui.item.attr("data-url");
+            var postAction = ui.item.attr("data-postLoadAction") + "()";
+            if (null !== url && url !== "") {
+                $.get(url, function (data) {
+                    $("#mainContentContainer").html(data);
+                    if (null !== postAction && postAction !== "" && postAction !== "undefined()") {
+                        eval(postAction);
+                    }
+                });
+            }
         }
     });
 });
+function _postFormData(formid, url) {
+    var form = $("#" + formid);
+    $.ajax({
+        type: "POST",
+        url: url,
+        data: form.serialize(),
+        cache: false,
+        success: function (data) {
+            $("#mainContentContainer").html(data);
+        }
+    });
+}
