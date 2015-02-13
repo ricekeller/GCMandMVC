ac1d1104c59bbd00122356d050b9e8505ce36fe3
ricekeller <ricekeller@gmail.com>
2015-02-13 17:34:58 -0600

    modal
    modal

--- ChatApp.Web/ChatApp.Web/Content/Scripts/Layout.js
+++ ChatApp.Web/ChatApp.Web/Content/Scripts/Layout.js
@@ -1,28 +1,52 @@
-﻿$(function () {
-    $("#menu").menu({
-        select: function (event, ui) {
-            var url = ui.item.attr("data-url");
-            var postAction = ui.item.attr("data-postLoadAction") + "()";
-            if (null !== url && url !== "") {
-                $.get(url, function (data) {
-                    $("#mainContentContainer").html(data);
-                    if (null !== postAction && postAction !== "" && postAction !== "undefined()") {
-                        eval(postAction);
-                    }
-                });
-            }
-        }
-    });
+﻿$(function ()
+{
+	$("#menu").menu({
+		select: function (event, ui)
+		{
+			var url = ui.item.attr("data-url");
+			var postAction = ui.item.attr("data-postLoadAction") + "()";
+			if (url && null !== url && url !== "")
+			{
+				$.get(url, function (data)
+				{
+					$("#mainContentContainer").html(data);
+					if (null !== postAction && postAction !== "" && postAction !== "undefined()")
+					{
+						eval(postAction);
+					}
+				});
+			}
+		}
+	});
 });
-function _postFormData(formid, url) {
-    var form = $("#" + formid);
-    $.ajax({
-        type: "POST",
-        url: url,
-        data: form.serialize(),
-        cache: false,
-        success: function (data) {
-            $("#mainContentContainer").html(data);
-        }
-    });
+function _postFormData(formid, url)
+{
+	var form = $("#" + formid);
+	$.ajax({
+		type: "POST",
+		url: url,
+		data: form.serialize(),
+		cache: false,
+		success: function (data)
+		{
+			$("#mainContentContainer").html(data);
+		}
+	});
+}
+function _showLoadingDiv()
+{
+	$("#overlay").dialog({
+
+	}).show();
+}
+
+function _hideLoadingDiv()
+{
+	$("#overlay").hide();
+}
+
+function overlay()
+{
+	el = document.getElementById("overlay");
+	el.style.visibility = (el.style.visibility == "visible") ? "hidden" : "visible";
 }
