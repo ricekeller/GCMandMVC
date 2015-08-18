57cb43233003b1c44af6abd8390718efa5a74bea
ricekeller <ricekeller@gmail.com>
2015-08-18 09:01:11 -0500

    server push
    server push

---
+++ ChatApp.Web/ChatApp.Web/Controllers/ChatApiController.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using ChatApp.Web.Models.Chat;
+using Newtonsoft.Json;
+
+namespace ChatApp.Web.Controllers
+{
+    public class ChatApiController : ApiController
+    {
+		private static readonly ConcurrentQueue<StreamWriter> _subscribers = new ConcurrentQueue<StreamWriter>();
+        // GET api/chat
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+
+        // GET api/chat/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+		public HttpResponseMessage Get(HttpRequestMessage request)
+		{
+			HttpResponseMessage response = request.CreateResponse();
+			response.Content = new PushStreamContent(OnStreamAvailable, "text/event-stream");
+			return response;
+		}
+
+		private static void OnStreamAvailable(Stream stream, HttpContent content, TransportContext context)
+		{
+			StreamWriter sw = new StreamWriter(stream);
+			_subscribers.Enqueue(sw);
+		}
+
+		public void Post(Message m)
+		{
+			m.Timestamp = DateTime.Now.ToString("MM/dd/yyyy HH:MM:SS");
+			ProcessMessage(m);
+		}
+
+		private static void ProcessMessage(Message m)
+		{
+			foreach(StreamWriter sw in _subscribers)
+			{
+				sw.WriteLine("data:" + JsonConvert.SerializeObject(m) + "n");
+				sw.Flush();
+			}
+		}
+        // POST api/chat
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/chat/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/chat/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ ChatApp.Web/ChatApp.Web/Models/Chat/Message.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ChatApp.Web.Models.Chat
+{
+	public class Message
+	{
+		public string Sender { get; set; }
+		public string Timestamp { get; set; }
+		public string MessageContent { get; set; }
+	}
+}
