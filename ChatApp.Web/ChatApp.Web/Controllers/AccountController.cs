1395ace53f387b2a5fa88edcba495b7a2450e5cf
ricekeller <ricekeller@gmail.com>
2015-10-12 09:09:48 -0500

    upperbar
    upperbar

--- ChatApp.Web/ChatApp.Web/Controllers/AccountController.cs
+++ ChatApp.Web/ChatApp.Web/Controllers/AccountController.cs
@@ -10,20 +10,54 @@ namespace ChatApp.Web.Controllers
 {
 	public class AccountController : Controller
 	{
-		//
-		// GET: /Account/
-
+		/// <summary>
+		/// Login action viewed with url
+		/// </summary>
+		/// <returns></returns>
 		public ActionResult Index()
 		{
 			return View();
 		}
 
+		/// <summary>
+		/// Register action viewed with url
+		/// </summary>
+		/// <returns></returns>
+		public ActionResult Signup()
+		{
+			return View();
+		}
+
+		/// <summary>
+		/// Logout
+		/// </summary>
+		/// <returns></returns>
+		public ActionResult Logout()
+		{
+			FormsAuthentication.SignOut();
+			return RedirectToAction("Index", "Home");
+		}
+
+		public ActionResult Profile()
+		{
+			return View();
+		}
+
+		/// <summary>
+		/// Login partial view
+		/// </summary>
+		/// <returns></returns>
 		[HttpGet]
 		public PartialViewResult Login()
 		{
 			return PartialView("_Login", new LoginViewModel());
 		}
 
+		/// <summary>
+		/// Login partial view post
+		/// </summary>
+		/// <param name="lvm"></param>
+		/// <returns></returns>
 		[HttpPost]
 		[ValidateAntiForgeryToken]
 		public PartialViewResult Login(LoginViewModel lvm)
@@ -40,12 +74,21 @@ namespace ChatApp.Web.Controllers
 			return PartialView("_Login", lvm);
 		}
 
+		/// <summary>
+		/// Register partial view
+		/// </summary>
+		/// <returns></returns>
 		[HttpGet]
 		public PartialViewResult Register()
 		{
 			return PartialView("_Register", new CreateAccountViewModel());
 		}
 
+		/// <summary>
+		/// Regsiter partial view post
+		/// </summary>
+		/// <param name="vm"></param>
+		/// <returns></returns>
 		[HttpPost]
 		[ValidateAntiForgeryToken]
 		public PartialViewResult Register(CreateAccountViewModel vm)
@@ -63,5 +106,7 @@ namespace ChatApp.Web.Controllers
 			}
 			return PartialView("_Register", vm);
 		}
+
+		
 	}
 }
